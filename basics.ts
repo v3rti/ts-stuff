9a93423af1697ff77950f6e9e453125ae0f4964d
Amine <med.bagui.1@gmail.com>
2024-03-02 01:26:19 +0000

    basics still

---
+++ basics.ts
@@ -0,0 +1,11 @@
+let user: string = "Amine";
+let hasLoggedIn: boolean = true;
+let num :number = 10;
+
+let regexVariable: RegExp = /bar/;
+
+const users: string[] = user.split(" ");
+
+hasLoggedIn += " a string";
+
+console.log(hasLoggedIn);
