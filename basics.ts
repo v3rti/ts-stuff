34ddcbceb86cd102cc9f51ad9555daf54148cdf7
Amine <med.bagui.1@gmail.com>
2024-03-03 00:09:20 +0000

    basics still, arrays, loops outputs, objects

--- basics.ts
+++ basics.ts
@@ -6,6 +6,38 @@ let regexVariable: RegExp = /bar/;
 
 const users: string[] = user.split(" ");
 
-hasLoggedIn += " a string";
+const values: Array<number> = [1,4,5,9];
 
-console.log(hasLoggedIn);
+const myUser: {firstName: string; lastName: string, isCoding: boolean} = {
+  firstName: "Amine",
+  lastName: "Saad",
+  isCoding: false
+}
+
+interface User {
+  firstName: string;
+  lastName: string;
+}
+
+const newUser: User = {
+  firstName: "Amine",
+  lastName: "Said"
+}
+
+const ids: Record<number, string> = {
+  15: "a",
+  30: "b"
+}
+
+ids[45] = "ab";
+
+if (ids[45] === "ab"){
+  
+}
+
+for (let i = 0; i < 5; i++){
+  console.log(i);
+}
+
+const output: number[] = [3,4,5].map((n) => n * 10);
+const secOutput: string[] = [3,4,5].map((n) => `${n * 10}`); /* output of the map is a string in this case unlike the one before */ 
